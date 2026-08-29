c051b431084225f25b12e563081d54e3672369fe
apatowj <joeapatow@gmail.com>
2026-08-29 10:09:14 -0400

    drew player shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from player import Player
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
 
 def main():
@@ -7,6 +8,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
@@ -17,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(("black"))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
             
